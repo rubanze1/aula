f637d56d33fad1192e298003453eced4cb86f9c0
rubanze1 <50624343+rubanze1@users.noreply.github.com>
2019-05-14 14:39:52 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<? php
+
+
+
+
+
+?>
+
+
+<html>
+
+<title> O seu cantinho de Entretenimento - Pagina Inicial</title>
+<body>
+
+
+
+<header>
+
+Deixa musica fluir pela sua <strong>mente</strong>
+<h3> Para os que a musica faz parte das suas vidas</h3>
+
+<header>
+
+
+
+
+
+
+
+
+
+<form action="sub.php">
+
+ <fieldset>
+    <legend>Bem vindo Acesse com as credencias</legend>
+  Usuario:<br>
+    <input type="Username" name="username" value="usuario">
+    <br>
+   Password <br>
+    <input type="Password" name="pass" value="password">
+    <br><br>
+	
+    <input type="button" value="Aceder">
+  </fieldset>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
